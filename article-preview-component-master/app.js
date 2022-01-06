aef78bcbc869e44b8d6d60feeff23d6e9bdb857d
johnsmccain <johnsdanlami@mail.com>
2022-01-07 00:35:30 +0100

    initial commit

--- article-preview-component-master/app.js
+++
@@ -1,7 +0,0 @@
-const toggler = document.querySelector(".share");
-const overlay = document.querySelector(".overlay");
-let truthy = true;
-toggler.onclick = () => {
-  truthy = !truthy;
-  overlay.className = `${truthy ? "overlay" : "hide "}`;
-};
